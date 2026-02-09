00f45dc604b877d78d2b71ae9bf24828857976a7
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-09 16:46:58 -0700

    Practice auditing code in terminal's debugger mode

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -43,11 +43,14 @@ function reverse(str) {
 function reverse(str) {
   // 1) Use split method to convert string to an array of characters,
   // 2) Initialize accumulator to an empty string and use reduce method to iterate over the array of characters
-  // 3) In callback of reduce, add each subsequent character to the beginning of the string, effectually reducing the original string passed in to the reverse function.
+  // 3) In callback of reduce, add each subsequent character to the beginning of the string, effectually reversing the original string passed in to the reverse function.
   // 4) Return the reversed string.
   return str.split("").reduce((accumulator, character) => {
+    debugger;
     return character + accumulator;
   }, "");
 }
+
+reverse("medieval");
 
 module.exports = reverse;
