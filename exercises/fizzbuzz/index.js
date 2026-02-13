1f1dd7e4d83c88bd20e7aab464a9742eedb08cd6
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-12 23:34:58 -0700

    Solve and annotate solution to fizzbuzz problem

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  // Loop over numbers from 1 to n. Initialize i to 1, increment i by 1 while i is less than or equal to n.
+  // "Else if" is used to exclude the other conditions and avoid misprintings, in case multiple conditions are successively met.
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      // If i is a multiple of 3 and 5, logically it is a multiple of 15. Print "fizzbuzz".
+      console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+      // Else, if i is a multiple of 5, print "buzz".
+      console.log("buzz");
+    } else if (i % 3 === 0) {
+      // Else, if i is a multiple of 3, print "fizz".
+      console.log("fizz");
+    } else {
+      // Else, print the value of i.
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
