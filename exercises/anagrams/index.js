3af1bfca25db7ffaa18ae1b657c16cf3b8a1b39e
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-02-19 00:54:56 -0700

    Annotate solution 1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -11,28 +11,30 @@
 function buildCharMap(str) {
   const charMap = {};
 
+  // Use a for...of loop to iterate over the characters of the string. The string has been modified using a regex so that punctuation and spaces are replaced with empty strings to eliminate them when comparing the character maps later.
   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
+    charMap[char] = charMap[char] + 1 || 1; // If found, increment the value at the given character (i.e. key) by 1; otherwise, initialize the undefined value to 1.
   }
 
-  return charMap;
+  return charMap; // Return the populated character map
 }
 
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
+  const aCharMap = buildCharMap(stringA); // Use helper function to build a character map of string A. The character map is an object.
+  const bCharMap = buildCharMap(stringB); // User helper function to build a character map of string B. The character map is an object.
 
   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
+    return false; // If the number of keys in each character map isn't equal, then the two strings passed into the anagrams function are NOT anagrams.
   }
 
+  // Use for...in loop because character map is an object.
   for (let char in aCharMap) {
     if (aCharMap[char] !== bCharMap[char]) {
-      return false;
+      return false; // If the value at a given character (i.e. key) of the character map doesn't match the value at the identical key in the other character map, then the two strings are NOT anagrams.
     }
   }
 
-  return true;
+  return true; // If the two cases above don't apply, then the two strings ARE anagrams.
 }
 
 module.exports = anagrams;
