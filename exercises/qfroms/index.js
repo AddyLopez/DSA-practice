92404f3287121582e9d54f3e48113caa1e0db2c1
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-03-16 23:59:27 -0600

    Annotate my solution with explanatory notes

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,6 +12,12 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
+// Notes:
+// Visualize one stack (FILO - First In Last Out / LIFO - Last In First Out) reversing its order by transferring its contents one by one to a second stack.
+// The second stack enables popping off the top which is the (FIFO - First In First Out) first in the queue
+// The while loops below take care of transferring elements (I imagine them as books) back and forth to preserve or reset the order of elements.
+// The peek method is used to figure out whether there are any remaining elements to iterate over.
+
 const Stack = require("./stack");
 
 class Queue {
