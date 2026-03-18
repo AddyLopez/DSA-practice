d7bcbfd04bd4c4b8998c37365a0b6ffef8c101fa
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-03-17 18:21:37 -0600

    Implement insertFirst method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,6 +13,11 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertFirst(data) {
+    const node = new Node(data, this.head); // Create new Node instance passing in data and assigning this.head to the next property of the node
+    this.head = node; // Reassign head property to new Node instance
+  }
 }
 
 module.exports = { Node, LinkedList };
