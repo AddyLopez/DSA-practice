23ead853394147eb3de0d2947fa9afac5cc2c29c
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2026-05-06 15:41:06 -0600

    Implement selectionSort algorithm

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -14,7 +14,26 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i; // ASSUME i is the index of the minimum value
+
+    for (let j = i + 1; j < arr.length; j++) {
+      // Iterate over elements to the right of i (i.e. to the right of the current index)
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j; // If there is an element less than the assumed minimum, then update the indexOfMin to the index of that element
+      }
+    }
+
+    // If i did not represent the mimimum's index, swap the two elements at i and at indexOfMin
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin]; // Retain this reference for a successful swap
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr; // Return sorted array (from least to greatest)
+}
 
 function mergeSort(arr) {}
 
